574899b9349a96739801bb7e00a0b56b0c432328
Khush Jain <khush4080@gmail.com>
2020-08-11 02:30:19 +0530

    Updates

---
+++ precache-manifest.c8373136786a5f3c27a17fb666638843.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9f3c02bcf8c7a0fed8eebc0efea8f9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8004fad0c50d583dfc77",
+    "url": "/portfolio/static/css/main.e2593a42.chunk.css"
+  },
+  {
+    "revision": "bc64b6409a23cf1550f6",
+    "url": "/portfolio/static/js/2.f49892ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f49892ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8004fad0c50d583dfc77",
+    "url": "/portfolio/static/js/main.fab46f11.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "48c48e27cb9754e4fc749242ea57c166",
+    "url": "/portfolio/static/media/bg.48c48e27.jpg"
+  },
+  {
+    "revision": "7aea1583e61fda7266b8d90852bcbc46",
+    "url": "/portfolio/static/media/display-pic.7aea1583.jpg"
+  }
+]);
